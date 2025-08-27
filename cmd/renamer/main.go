f64b141aeee381bd31dc4359711af44d74dec344
liasica <magicrolan@qq.com>
2025-08-27 19:10:30 +0800

    添加invoke

--- cmd/renamer/main.go
+++ cmd/renamer/main.go
@@ -17,7 +17,7 @@ import (
 // https://help.aliyun.com/zh/functioncompute/fc-3-0/user-guide/context-and-log-format-1?spm=5176.fcnext.console-base_help.dexternal.1aa32f03mxVDCk
 func main() {
 	e := echo.New()
-	e.GET("/", func(c echo.Context) error {
+	e.POST("/invoke", func(c echo.Context) error {
 		for s, ss := range c.Request().Header {
 			fmt.Printf("%s: %s\n", s, strings.Join(ss, " | "))
 		}
